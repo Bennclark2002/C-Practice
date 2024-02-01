064f744c01de96b3ed50c265dca424464d3a8d38
Bennclark2002 <92816199+Bennclark2002@users.noreply.github.com>
2024-02-01 11:48:51 -0600

    Add files via upload

---
+++ Book/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Transactions;
+
+class Q2
+{
+    static void Main(string[] args)
+    {
+        Book myBook = new Book("Pride and Prejudice", "Jane Austen", 5.97, "0141439513");
+        string bookinfo = myBook.PrintInformation();
+        Console.WriteLine(bookinfo);
+    }
+}
+
+public class Book
+{
+    private string title;
+    private string author;
+    private double price;
+    private string isbn;
+
+    public Book(string title, string author, double price, string isbn)
+    {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+        this.isbn = isbn;
+    }
+
+    public string Title
+    {
+        get { return title; }
+        set { title = value; }
+    }
+
+    public string Author
+    {
+        get { return author; }
+        set { author = value; }
+    }
+
+    public double Price
+    {
+        get { return price; }
+        set { price = value; }
+    }
+
+    public string ISBN
+    {
+        get { return isbn; }
+        set { isbn = value; }
+    }
+
+    public string PrintInformation()
+    {
+        return $"{title} written by {author} is {price:C2}, with ISBN {isbn}.";
+    }
+}
---
+++ Debug_Exercise/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+
+namespace Q1
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            Test mytest = new Test(0);
+
+            mytest.AddEight();
+
+            mytest.Display();
+
+        }
+    }
+
+    public class Test
+    {
+        private int tally;
+        public Test(int tally)
+        {
+            this.tally = tally;
+        }
+
+        public void AddEight()
+        {
+            tally += 8;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("tally is {0}", tally);
+        }
+    }
+}
