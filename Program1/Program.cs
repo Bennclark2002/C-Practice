0c84c7797ef98c88c1032e97f9aa7567ad8a76f8
Bennclark2002 <92816199+Bennclark2002@users.noreply.github.com>
2024-02-01 11:50:11 -0600

    Add files via upload

---
+++ Program1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Program1
+{
+    class PrimeNumbers
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter a Number: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Prime numbers between 2 and " + n + ": ");
+            for (int i = 2; i <= n; i++)
+            {
+                if (IsPrime(i))
+                    Console.Write(i + " "); 
+            }
+            Console.ReadLine();
+        }
+
+        public static bool IsPrime(int number)
+        {
+            if (number <= 1) return false; // checks to see if it is less than or equal to 1 and if its then it is false
+            if (number == 2) return true; // checks to see if the input is equal to 2 and if it is then it is returned as the only prime number
+            if (number % 2 == 0) return false; // checks to see if the input is divisable by 2
+
+            var boundary = (int)Math.Floor(Math.Sqrt(number)); 
+
+            for (int i = 3; i <= boundary; i += 2) // 
+            {
+                if (number % i == 0)
+                    return false;
+            }
+            return true;
+        }
+    }
+}
---
+++ Program2/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.ComponentModel.Design;
+
+namespace SortingNumbers
+{
+    class Program2
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of elements: ");
+            int numElements = Convert.ToInt32(Console.ReadLine());
+
+            float[] numbers = new float[numElements];
+
+            Console.Write("Enter the elements separagted by space: ");
+            string[] inputNumbers = Console.ReadLine().Split(' ');
+
+            for (int i = 0; i < numElements; i++)
+            {
+                numbers[i] = float.Parse(inputNumbers[i]);
+            }
+
+            SelectionSort(numbers);
+
+            Console.Write("Sorted numbers: ");
+            for (int i = 0; i < numElements; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+            Console.ReadLine();
+        }
+
+        private static void SelectionSort(float[] numbers)
+        {
+            int minIndex;
+
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                minIndex = i;
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[j] < numbers[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+
+                Swap(ref numbers[minIndex], ref numbers[i]);
+            }
+        }
+
+        private static void Swap(ref float a, ref float b)
+        {
+            float temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
+
